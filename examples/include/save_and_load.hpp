16573ee7b504d39cbee41ef4133e8cf7bd48765c
kades <kades@thphys.uni-heidelberg.de>
2020-12-08 11:48:20 +0100

    examples files

---
+++ examples/include/save_and_load.hpp
@@ -0,0 +1,57 @@
+//
+// Created by lukas on 04.12.20.
+//
+
+#ifndef MCMCSIMULATIONLIB_STORING_AND_LOADING_HPP
+#define MCMCSIMULATIONLIB_STORING_AND_LOADING_HPP
+
+#include "../../include/mcmc_simulation/header.hpp"
+#include "../../include/execution/modes/correlation_time.hpp"
+
+// For systembases with custom measures
+#include "../../templates/systembase_template.hpp"
+
+// For systembases with the possible usage of predefined common measures
+#include "../../templates/systembase_measures_template.hpp"
+
+/* Save simulating parameters and load simulation from file - Execution mode: CorrelationTime */
+
+void save_and_load() {
+    // Setting up system parameters
+    SystemBaseTemplateParameters system_params(json {{"running_parameter", 0.0}});
+
+
+    // Setting up execution parameters
+    typedef mcmc::execution::CorrelationTimeParameters CorrelationTimeParams;
+    CorrelationTimeParams correlation_time_execution_parameters(10, 100, 100);
+
+
+    // Setting up simulation parameters
+    std::string target_name = "StoringAndLoading";
+    std::string rel_data_path = "/data/" + target_name + "/";
+    auto simulation_params = mcmc::simulation::SimulationParameters< SystemBaseTemplateParameters , CorrelationTimeParams >::generate_simulation(
+            system_params, correlation_time_execution_parameters, rel_data_path,
+            "systembase_params", "running_parameter", 0.0, 1.0, 5);
+
+
+    // Store execution and simulation parameters as config files for a possible later simulation - In this case,
+    // the expectation values are stored also automatically since they are part of simulation parameters
+    std::string rel_sim_params_path = "/configs/" + target_name + "/";
+    simulation_params.write_to_file(rel_sim_params_path);
+
+
+    std::cout << "\nAll necessary parameters for the simulation have been written to rel_config_path" << std::endl;
+
+
+    // Load simulation parameters from file - Note: It is important that the template parameters coincide for storing and loading
+    auto from_file_simulation_params = mcmc::simulation::SimulationParameters< SystemBaseTemplateParameters , CorrelationTimeParams >::generate_simulation_from_file(
+            rel_sim_params_path);
+
+    std::cout << "\nFrom file simulation params: \n" << from_file_simulation_params << std::endl;
+
+    // Setting up and running the actual simulation
+    mcmc::simulation::Simulation< SystemBaseTemplateParameters, CorrelationTimeParams > from_file_simulation(simulation_params);
+    from_file_simulation.run();
+}
+
+#endif //MCMCSIMULATIONLIB_STORING_AND_LOADING_HPP
---
+++ examples/src/main.cpp
@@ -0,0 +1,99 @@
+//
+// Created by lukas on 04.12.20.
+//
+
+#include "mcmc_simulation/header.hpp"
+
+#include "execution/modes/expectation_value.hpp"
+#include "execution/modes/correlation_time.hpp"
+#include "execution/executer.hpp"
+
+// For systembases with custom measures
+#include "../../templates/systembase_template.hpp"
+
+// For systembases with the possible usage of predefined common measures
+#include "../../templates/systembase_measures_template.hpp"
+
+#include "../include/config.h"
+
+typedef SystemBaseMeasuresTemplateParameters SystemBaseParams;
+
+void custom_main();
+
+int main(int argc, char **argv) {
+    param_helper::fs::prfs::set_relative_path_to_project_root_dir("/../");
+
+#ifdef PYTHON
+    mcmc::execution::initialize_python();
+#endif
+
+    // Initialization
+    mcmc::execution::initialize_executer_params(PROJECT_NAME, CLUSTER_MODE, CONDA_ACTIVATE_PATH, VIRTUAL_ENV, PYTHON_SCRIPTS_PATH);
+
+    // A function of one of the first three if conditions is only called when an actual simulation takes place
+    // (or for the generation of default parameters) based on a program that uses ./Main with arguments (from cpu/gpu/locally)
+    if(argc > 1)
+    {
+        mcmc::execution::run_from_file<SystemBaseParams>(argc, argv);
+    }
+    else
+        // Helpful for a preparation of the simulation or immediate execution (on cpu/gpu/locally, testing/running directly)
+        custom_main();
+
+    // Finalization
+#ifdef PYTHON
+    mcmc::execution::finalize_python();
+#endif
+    return 0;
+
+}
+
+#include "../include/simple_simulation.hpp"
+#include "../include/save_and_load.hpp"
+#include "../include/ising_model.hpp"
+
+void custom_main() {
+    simple_simulation();
+    save_and_load();
+    ising_model_simulation();
+    /* // Setting up system parameters - This does also work with SystemBaseMeasureTemplateParameters
+    SystemBaseParams system_params(json {{"running_parameter", 0.0}});
+
+
+    // Setting up execution parameters - "measures" can only be defined here if SystemBaseMeasuresTemplateParameters is used
+    typedef mcmc::execution::ExpectationValueParameters ExpectationValueParams;
+    ExpectationValueParams execution_parameters(100, 10000, 100, {}, {});
+
+
+    // Setting up simulation parameters
+    std::string target_name = "HelloWorldSimulation";
+    std::string rel_data_path = "/data/" + target_name + "/";
+    auto simulation_params = mcmc::simulation::SimulationParameters< SystemBaseParams , ExpectationValueParams >::generate_simulation(
+            system_params, execution_parameters, rel_data_path,
+            "systembase_params", "running_parameter", 0.0, 1.0, 5);
+
+    // Store execution and simulation parameters as config files for a possible later simulation - In this case,
+    // the expectation values are stored also automatically since they are part of simulation parameters
+    std::string rel_sim_params_path = "/configs/" + target_name + "/";
+    simulation_params.write_to_file(rel_sim_params_path);
+
+    // Using execute to execute the code directly
+    mcmc::execution::execute<SystemBaseParams>(
+            ExpectationValueParams::name(), // = "expectation_value"
+            target_name, // = "HelloWorldSimulation"
+            "/./", // default
+            true, // default
+            mcmc::execution::Executer::local, // mcmc::execution::Executer::local can be used to test the code first locally
+            true); // In this case, the bash script can be started with: qsub ... or nice -n 17 bash ...
+    */
+
+    // ToDo: Generate additional template for simulation with measurements
+
+    // ToDo: Generate example with Ising model
+
+    // ToDo: Add example with custom execution mode
+
+    // ToDo: General ToDo:
+    //  - Adapt lattice to not depend on rel_config anymore
+    //  - Think about adding functions for setting certain parameter paths explicitly -> or a constructor
+}
