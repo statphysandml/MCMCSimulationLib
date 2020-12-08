1e4857d5c6dfad50b876fdb7db1b457fbf49c8bb
kades <kades@thphys.uni-heidelberg.de>
2020-12-08 12:30:06 +0100

    update

--- build/generate_cmake_lists_txt_file.sh
+++ build/generate_cmake_lists_txt_file.sh
@@ -1,29 +1,13 @@
+target_link_libraries_appendix="\${ParamHelper}"
 cat >../CMakeLists.txt <<EOL
 cmake_minimum_required(VERSION 3.13)
 project(MCMCSimulationLib)
 
 set(CMAKE_CXX_STANDARD 14)
 
-# Boost
-EOL
-if [ -v path_to_boost ]; then
-cat >>../CMakeLists.txt <<EOL
-set(BOOST_ROOT "${path_to_boost}")
-FIND_PACKAGE( Boost REQUIRED COMPONENTS filesystem)
-EOL
-else
-cat >>../CMakeLists.txt <<EOL
-FIND_PACKAGE( Boost 1.67 REQUIRED COMPONENTS filesystem)
-if(Boost_FOUND)
-    include_directories(\${Boost_INCLUDE_DIRS})
-    message("Boost = \${Boost_INCLUDE_DIRS}")
-endif()
-EOL
-fi
-cat >>../CMakeLists.txt <<EOL
-
 EOL
 if [ -v path_to_python3 ]; then
+target_link_libraries_appendix="${target_link_libraries_appendix} \${PYTHON_LIBRARIES}"
 cat >>../CMakeLists.txt <<EOL
 # Python
 set(PYTHON_LIBRARIES "${path_to_python3}lib/libpython${python_version}m.so")
@@ -35,11 +19,12 @@ find_package(PythonLibs 3 REQUIRED)
 find_package(Python3 REQUIRED COMPONENTS Interpreter Development)
 include_directories(\${PYTHON_INCLUDE_DIRS})
 message("Python executable = \${PYTHON_EXECUTABLE}")
-option( PYTHON "Enable Python" ON)
+
+option(PYTHON "Enable Python" ON)
 EOL
 else
 cat >>../CMakeLists.txt <<EOL
-option( PYTHON "Disable Python" OFF)
+option(PYTHON "Disable Python" OFF)
 EOL
 fi
 cat >>../CMakeLists.txt <<EOL
@@ -58,13 +43,8 @@ add_library(mcmcsimulationlib STATIC
 find_library(ParamHelper NAMES libparamhelper.a PATHS ${path_to_param_helper}lib)
 message("ParamHelper = \${ParamHelper}")
 include_directories(${path_to_param_helper}include/)
+target_link_libraries(mcmcsimulationlib ${target_link_libraries_appendix})
 
-EOL
-if [ -v path_to_python3 ]; then
-cat >>../CMakeLists.txt <<EOL
-target_link_libraries(mcmcsimulationlib \${ParamHelper} \${Boost_LIBRARIES} \${PYTHON_LIBRARIES})
-EOL
-ficat >>../CMakeLists.txt <<EOL
 
 SET( APP_EXE StaticTest )
 
