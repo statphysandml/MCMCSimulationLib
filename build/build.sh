13fd68d2587afa85f2d6a91412d801b7cfdcac5a
kades <kades@thphys.uni-heidelberg.de>
2020-12-10 19:59:36 +0100

    update

--- build/build.sh
+++ build/build.sh
@@ -3,8 +3,6 @@ if [ -z ${path_to_config+x} ]; then
   path_to_config="$(dirname -- "$(readlink -f -- "build.sh")")"
 fi
 
-path_to_base_lib=${path_to_mcmc_simulation_lib}
-
 # Verify if config.sh file has been generated
 if ! test -f "${path_to_config}/config.sh"; then
   echo "config.sh file not in build directory. You can copy the config_template.sh file and adapt the parameters according to your system."
