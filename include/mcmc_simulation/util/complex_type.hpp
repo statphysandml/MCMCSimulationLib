1d9d5619ee5d4e186c91bd6f4b11f94c976b587e
kades <kades@thphys.uni-heidelberg.de>
2020-12-12 21:10:31 +0100

    update

---
+++ include/mcmc_simulation/util/complex_type.hpp
@@ -0,0 +1,25 @@
+//
+// Created by lukas on 12.12.20.
+//
+
+#ifndef MCMCSIMULATIONLIB_COMPLEX_TYPE_HPP
+#define MCMCSIMULATIONLIB_COMPLEX_TYPE_HPP
+
+#include "complex"
+
+namespace std
+{
+    template<typename T>
+    std::string to_string(std::complex<T> x)
+    {
+        return std::to_string(x.real()) + " i" + std::to_string(x.imag());
+    }
+
+    /* template<typename T>
+    double fabs(std::complex<T> x)
+    {
+        return std::fabs(x);
+    } */
+}
+
+#endif //MCMCSIMULATIONLIB_COMPLEX_TYPE_HPP
