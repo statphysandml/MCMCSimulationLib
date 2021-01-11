b0caf9b4709b30c42165f38ae7836fc866b713e4
kades <kades@thphys.uni-heidelberg.de>
2021-01-11 15:18:33 +0100

    added deleting lib if rebuild

--- build/build_library.sh
+++ build/build_library.sh
@@ -3,6 +3,12 @@
 parent_dir="$(dirname -- "$(readlink -f -- "build_project.sh")")"
 path_to_mcmc_simulation_lib="$(dirname "$parent_dir")"
 
+# Remove lib directory if already existing to avoid errors when changing parameters
+if [ -d "${path_to_mcmc_simulation_lib}/lib" ]; then
+  echo "Remove lib folder to avoid errors due to changing parameters."
+  rm -r "${path_to_mcmc_simulation_lib}/lib"
+fi
+
 # Submodules
 path_to_param_helper="${path_to_mcmc_simulation_lib}/external_submodules/ParamHelper/"
 
