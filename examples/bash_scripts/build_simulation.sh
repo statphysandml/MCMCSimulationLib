7cefd5952e4b159d9aedaba77f27fe3a4063c5ad
kades <kades@thphys.uni-heidelberg.de>
2021-01-13 21:45:31 +0100

    added example bash_scripts

---
+++ examples/bash_scripts/build_simulation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+path_to_mcmc_simulation_lib="./../../"
+path_to_mcmc_simulation_lib="$(cd "$path_to_mcmc_simulation_lib" && pwd -P)"
+source "${path_to_mcmc_simulation_lib}/bash_scripts/build_simulation.sh"
