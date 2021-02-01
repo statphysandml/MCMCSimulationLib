c502a3a8d759bbe2fc9650cf658a09d6dabcfb6e
kades <kades@thphys.uni-heidelberg.de>
2021-02-01 22:45:18 +0100

    update minor change

--- examples/python_scripts/examples/expectation_value.py
+++ examples/python_scripts/examples/expectation_value.py
@@ -9,6 +9,10 @@ if __name__ == '__main__':
     # Navigate to examples/ directory as project root directory
     os.chdir("../../")
 
+    # Add python_scripts path to sys.path to include it as a module which contains custom_measures.py
+    import sys
+    sys.path.append("./python_scripts/")
+
     # Also used by the C++ MCMCSimulationLib
     expectation_value(files_dir="IsingModelSimulation")
 
